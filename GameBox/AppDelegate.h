6d469923f9b36b31349c79e9611621d1b8e0c6c3
qyzhaojinxi <qyzhaojinxi@163.com>
2015-02-09 09:37:09 +0800

    Initial Commit

---
+++ GameBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameBox
+//
+//  Created by 赵 进喜 on 15/2/9.
+//  Copyright (c) 2015年 everzones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GameBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameBox
+//
+//  Created by 赵 进喜 on 15/2/9.
+//  Copyright (c) 2015年 everzones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
